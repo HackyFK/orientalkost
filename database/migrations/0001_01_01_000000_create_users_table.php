d46ff990ec2ec970028f3d39ccb6b5d94ca20f23
HackyFK <hacky.fakke@gmail.com>
2026-02-25 13:38:36 +0700

    kelola user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,12 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('tipe_identitas', ['nik', 'nisn', 'nim', 'paspor']);
             $table->string('nomor_identitas')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
 
             $table->enum('role', ['admin', 'owner', 'customer'])->default('customer');
+            $table->boolean('is_active')->default(true);
+
             $table->string('phone')->nullable();
             $table->string('alamat')->nullable();
 
